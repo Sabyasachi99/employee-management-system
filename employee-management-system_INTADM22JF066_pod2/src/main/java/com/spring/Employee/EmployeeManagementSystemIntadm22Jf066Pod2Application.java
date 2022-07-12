f93ff14a43ae09b913ecc407acefec71c10dca0b
Sabyasachi Das <87321296+Sabyasachi99@users.noreply.github.com>
2022-07-12 23:58:25 +0530

    Add files via upload

---
+++ employee-management-system_INTADM22JF066_pod2/src/main/java/com/spring/Employee/EmployeeManagementSystemIntadm22Jf066Pod2Application.java
@@ -0,0 +1,13 @@
+package com.spring.Employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementSystemIntadm22Jf066Pod2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementSystemIntadm22Jf066Pod2Application.class, args);
+	}
+
+}
---
+++ employee-management-system_INTADM22JF066_pod2/src/test/java/com/spring/Employee/EmployeeManagementSystemIntadm22Jf066Pod2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.Employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemIntadm22Jf066Pod2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
